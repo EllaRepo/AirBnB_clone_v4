20db51592a6864ea25751b83bbe6edbdcc45ed13
Eliab E <eliaberango@gmail.com>
2023-10-08 13:46:13 -0700

    Fix semistandard error

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,12 @@
+const $ = window.$;
 $('document').ready(function () {
-    let amenities = {};
-    $('input[type="checkbox"]').change(function () {
-      if ($(this).is(':checked')) {
-        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-      } else {
-        delete amenities[$(this).attr('data-id')];
-      }
-      $('.amenities h4').text(Object.values(amenities).join(', '));
-    });
+  const amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenities).join(', '));
   });
+});
