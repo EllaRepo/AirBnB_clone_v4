725d06fdda0559d7cdb480acef7f3ee18ab0e53a
Eliab E <eliaberango@gmail.com>
2023-10-09 12:01:00 -0700

    Refactored

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,23 +1,13 @@
 const $ = window.$;
 $(document).ready(function () {
-  const myAmenities = {};
-  let myList = [];
-  const checkbox = $('.amenities input[type="checkbox"]');
-  checkbox.prop('checked', false);
-  checkbox.change(function () {
-    const dataId = $(this).attr('data-id');
-    const dataName = $(this).attr('data-name');
-    if (this.checked) {
-      myAmenities[dataId] = dataName;
+  const amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      delete (myAmenities[dataId]);
+      delete amenities[$(this).attr('data-id')];
     }
-    for (const key in myAmenities) {
-      myList.push(myAmenities[key]);
-    }
-    const output = myList.join(', ');
-    $('div.amenities > h4').text(output);
-    myList = [];
+    $('.amenities h4').text(Object.values(amenities).join(', '));
   });
 
   const apiStatus = $('div#api_status');
